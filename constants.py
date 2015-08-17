71b262569e78db2c6370e8775a8c7c398b9f1b1d
Dustin <dustin.wyatt@gmail.com>
2015-08-17 10:07:40 -0500

    Move constants to constants.py

---
+++ constants.py
@@ -0,0 +1,13 @@
+__author__ = 'Dustin'
+BASIC_HEADERS = (
+    ('Host', 'web9.secureinternetbank.com'),
+    ('Origin', 'https://web9.secureinternetbank.com'),
+    ('User-Agent',
+     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.26 Safari/537.36')
+)
+BEAUTIFULSOUP_PARSER = 'html.parser'
+KEYRING_SERVICE_NAME = 'belgrade'
+KEYRING_QUESTIONS_KEY = 'questions'
+KEYRING_USERNAME_KEY = 'username'
+BASE_URL = "https://web9.secureinternetbank.com/pbi_pbi1961/"
+ACCT_LIST_URL = "https://web9.secureinternetbank.com/pbi_pbi1961/PBI1961.ashx?SPTN=2E4DBA2FF93E4383ADDFDF296FC1B15A&WCI=AcctLst"
